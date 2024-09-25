ebabaf266f2a343d0f1fa280e3b4c70ef9f95753
Diego R Cristaldo <diegorcristaldo@hotmail.com>
2024-09-25 11:50:42 -0300

    Concluindo projeto de POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("(19)98990-9456", "G20Plus", "156489462008", 512);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Iphone iphone = new Iphone("(67)98990-9456", "GiBaito", "03684160687", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Onlyfans");
